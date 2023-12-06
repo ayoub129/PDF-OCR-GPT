6a2e246fac0edc9c1df220c31a11d5dcccc1d216
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2023-12-06 16:42:54 +0100

    Update PerformOcr.php

--- PerformOcr.php
+++ PerformOcr.php
@@ -7,7 +7,7 @@ function performOCR($imagePath)
 {
 
     // Specify the path to the Tesseract executable
-    $tesseractExecutable = __DIR__ . 'Tesseract-OCR\tesseract.exe';
+    $tesseractExecutable = DIR . '\Tesseract-OCR\tesseract.exe';
 
     // Output file path for the OCR result with timestamp
     $outputFile = 'output';
@@ -15,11 +15,14 @@ function performOCR($imagePath)
     // Run Tesseract OCR
     $command = "{$tesseractExecutable} {$imagePath} {$outputFile}  -l ita";
     shell_exec($command);
+    echo $command;
 
 
     // Read and return the OCR result
     $ocrResult = file_get_contents($outputFile . ".txt");
+    echo $ocrResult;
     echo file_get_contents($outputFile . ".txt");
 
     return $ocrResult;
 }
+
